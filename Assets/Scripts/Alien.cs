a7e30701deab5c19dbb0d3d99903b2e257674c1b
William Lalonde <Sir-Will03@outlook.com>
2022-09-30 11:03:57 -0400

    Chp5 added NavMesh, Alien Script

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Alien : MonoBehaviour
 {
+    public Transform target;
+    public float navigationUpdate;
+    private float navigationTime = 0;
+    private NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        navigationTime += Time.deltaTime;
+
+        if(navigationTime > navigationUpdate)
+        {
+            agent.destination = target.position;
+            navigationTime = 0;
+        }
+
+        if(target != null)
+        {
+            agent.destination = target.position;
+        }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
 }
